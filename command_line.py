231a975ed9d38cda635afb4a497f46330ec5cb46
SmoothieGames <SmoothieGamesBusiness@outlook.com>
2026-01-22 15:44:39 -0600

    Testing

--- command_line.py
+++ command_line.py
@@ -10,8 +10,22 @@ This will be the entry point for the project when run from the command line.
 '''Function for getting the data in the column "Industrial Water Use (%)"'''
 
 '''Function for getting the data in the column "Household Water Use (%)"'''
+import csv
 #FUNCTION 2
 
 #FUNCTION 3
 
 #MAIN
+
+def main():
+    loadDataSparseCountry()
+def loadDataSparseCountry():
+    arr = []
+    with open('Data/AQUASTAT-Water Use.csv',newline='') as csvfile:
+        reader = csv.reader(csvfile,delimiter = ',',quotechar="|")
+        for row in reader:
+            if row[4] == "United States of America":
+                print(str(row))
+
+if __name__=="__main__":
+    main()
