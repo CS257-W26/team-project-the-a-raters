48dd6dfe3f55658db4e0e3d8eca29111f1ec44b1
SmoothieGames <SmoothieGamesBusiness@outlook.com>
2026-01-26 17:02:59 -0600

    Formatting Pass 1

--- Tests/test_command_line.py
+++ Tests/test_command_line.py
@@ -1,28 +1,33 @@
-'''
+"""
 Test command line file
-'''
+"""
 
 import unittest
 from io import StringIO
 
-from command_line import * #note, specify this late
-#Running the line above is giving me an error.
+from command_line import *  # note, specify this late
+
+# Running the line above is giving me an error.
+
 
 class openDBTest(unittest.TestCase):
     def test_odb(self):
         arr = openDB(DB.CLEANED_GWC)
-        self.assertEqual(arr[0][0],"Country")
+        self.assertEqual(arr[0][0], "Country")
+
     def test_invalid_db(self):
-        self.assertRaises(KeyError,openDB,"AAAAAA")
+        self.assertRaises(KeyError, openDB, "AAAAAA")
+
 
 class filterTagsDBTest(unittest.TestCase):
     def test_base(self):
-        arr = filterTagsDB(DB.AQS_DS3,["United States of America"])
+        arr = filterTagsDB(DB.AQS_DS3, ["United States of America"])
         self.assertTrue(len(arr) > 0)
 
     def test_invalid_tag(self):
-        arr = filterTagsDB(DB.AQS_DS3,["THIS COUNTRY DOES NOT EXIST LOLOLOLOL"])
+        arr = filterTagsDB(DB.AQS_DS3, ["THIS COUNTRY DOES NOT EXIST LOLOLOLOL"])
         self.assertTrue(len(arr) == 0)
+
 
 class commandLineTest(unittest.TestCase):
     def _run_and_return_output(self) -> str:
@@ -30,33 +35,44 @@ class commandLineTest(unittest.TestCase):
         sys.stdout = StringIO()
         main()
         return sys.stdout.getvalue().strip()
+
     def test_run(self):
         sys.argv = []
         printed_out = self._run_and_return_output()
         self.assertEqual(printed_out, "USAGE STATEMENT GOES HERE")
 
     def test_run_waterUseTimeCompare(self):
-        sys.argv = ['command_line.py','-usageovertime', 'USA', '2001', '2003']
+        sys.argv = ["command_line.py", "-usageovertime", "USA", "2001", "2003"]
         printed_out = self._run_and_return_output()
-        self.assertEqual(printed_out,"Water usage in United States of America \n\n2001: 1829x10^9 cubic meters/year\n2003: 1829x10^9 cubic meters/year\nDifference:\n0x10^9 cubic meters/year")
+        self.assertEqual(
+            printed_out,
+            "Water usage in United States of America \n\n2001: 1829x10^9 cubic meters/year\n2003: 1829x10^9 cubic meters/year\nDifference:\n0x10^9 cubic meters/year",
+        )
 
     def test_run_waterUseTimeCompare_fail(self):
-        sys.argv = ['command_line.py','-usageovertime', 'USA', '2001']
+        sys.argv = ["command_line.py", "-usageovertime", "USA", "2001"]
         printed_out = self._run_and_return_output()
-        self.assertEqual(printed_out,"Invalid arguments.")
+        self.assertEqual(printed_out, "Invalid arguments.")
 
     def test_run_waterUseTimeCompare_invalid_year(self):
-        sys.argv = ['command_line.py','-usageovertime', 'USA', '2001','243243125245324']
-        self.assertRaises(KeyError,self._run_and_return_output)
+        sys.argv = [
+            "command_line.py",
+            "-usageovertime",
+            "USA",
+            "2001",
+            "243243125245324",
+        ]
+        self.assertRaises(KeyError, self._run_and_return_output)
+
 
 class perCapitaWaterUseTest(unittest.TestCase):
     def test_per_capita_valid(self):
-        '''Test valid country and year inputs'''
+        """Test valid country and year inputs"""
         result = get_per_capita_water_use("Japan", "2018")
         self.assertAlmostEqual(result, 290.58, places=2)
 
     def test_per_capita_invalid_country(self):
-        '''Test invalid country input'''
+        """Test invalid country input"""
         with self.assertRaises(ValueError):
             get_per_capita_water_use("Wakanda", "2018")
 
@@ -64,26 +80,28 @@ class perCapitaWaterUseTest(unittest.TestCase):
         with self.assertRaises(ValueError):
             get_per_capita_water_use("Japan", "1000")
 
+
 class usagePercentageTest(unittest.TestCase):
     def test_percentage_valid(self):
-        '''Test valid country/year/type input'''
+        """Test valid country/year/type input"""
         result = get_usage_percentage("Argentina", "2023", "Agricultural")
         self.assertAlmostEqual(result, 40.842, places=2)
 
     def test_percentage_invalid_country(self):
-        '''Test invalid country input'''
+        """Test invalid country input"""
         with self.assertRaises(ValueError):
             get_usage_percentage("Wakanda", "2023", "Agricultural")
 
     def test_percentage_invalid_year(self):
-        '''Test invalid year input'''
+        """Test invalid year input"""
         with self.assertRaises(ValueError):
             get_usage_percentage("Argentina", "3023", "Agricultural")
 
     def test_percentage_invalid_usage_type(self):
-        '''Test invalid usage type input'''
+        """Test invalid usage type input"""
         with self.assertRaises(ValueError):
             get_usage_percentage("Argentina", "2023", "Extrajudicial")
+
 
 class usageProportionTest(unittest.TestCase):
     def _run_and_return_output(self) -> str:
@@ -91,13 +109,16 @@ class usageProportionTest(unittest.TestCase):
         sys.stdout = StringIO()
         main()
         return sys.stdout.getvalue().strip()
-    
+
     def test_proportion_valid(self):
-        '''Test valid country/year/type input'''
-        sys.argv = ['command_line.py','-usageproportion', 'Argentina', '2024']
+        """Test valid country/year/type input"""
+        sys.argv = ["command_line.py", "-usageproportion", "Argentina", "2024"]
         printed_out = self._run_and_return_output()
-        self.assertEqual(printed_out,"Water usage in Argentina in 2024\n\nAgricultural:  51.20142857\nIndsutrial:  34.14285714\nHousehold:  23.52285714\nNone")
-    
+        self.assertEqual(
+            printed_out,
+            "Water usage in Argentina in 2024\n\nAgricultural:  51.20142857\nIndsutrial:  34.14285714\nHousehold:  23.52285714\nNone",
+        )
+
     def test_proportion_invalid_country(self):
         with self.assertRaises(ValueError):
             usageProportion("Wakanda", "2023")
@@ -105,16 +126,12 @@ class usageProportionTest(unittest.TestCase):
     def test_proportion_invalid_year(self):
         with self.assertRaises(ValueError):
             usageProportion("Argentina", "3023")
-    
+
     def test_proportion_no_year(self):
-        sys.argv = ['command_line.py','-usageproportion', 'Argentina']
+        sys.argv = ["command_line.py", "-usageproportion", "Argentina"]
         printed_out = self._run_and_return_output()
         self.assertRaises(KeyError, printed_out)
-    
-    
-            
 
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
--- command_line.py
+++ command_line.py
@@ -1,62 +1,69 @@
-'''
+"""
 The eventual location for the command line interface (CLI) for the project.
 This will be the entry point for the project when run from the command line.
-'''
+"""
 
-'''Function for getting the data cooresponding to a specific country/date combination'''
+import csv
+import sys
+from enum import Enum
+
+
+"""Function for getting the data cooresponding to a specific country/date combination"""
 
 '''Function for getting the data in the column "Agricultural Water Use (%)"'''
 
 '''Function for getting the data in the column "Industrial Water Use (%)"'''
 
 '''Function for getting the data in the column "Household Water Use (%)"'''
-import csv
-import sys
-from enum import Enum
-#FUNCTION 2
 
-#FUNCTION 3
 
-#MAIN
+# FUNCTION 2
+
+# FUNCTION 3
+
+
+# MAIN
 class DB(Enum):
+    """Enum for databases"""
+
     AQS_DS3 = "Data/AQUASTAT Dissemination System (3).csv"
     AQS_DS6 = "Data/AQUASTAT Dissemination System (6).csv"
     AQS_WR = "Data/AQUASTAT-Water Resources.csv"
     AQS_WU = "Data/AQUASTAT-Water Use.csv"
-    CLEANED_GWC = "Data/cleaned_global_water_consumption 2.csv" 
+    CLEANED_GWC = "Data/cleaned_global_water_consumption 2.csv"
+
 
 def main():
+    """Main func"""
     if len(sys.argv) <= 1:
         print("USAGE STATEMENT GOES HERE")
         return
     mode = sys.argv[1]
-    match mode.lower(): #These are examples. Feel free to change them.
+    match mode.lower():  # These are examples. Feel free to change them.
         case "-usageovertime":
             if len(sys.argv) <= 4:
                 print("Invalid arguments.")
                 return "Invalid arguments."
-            waterUseTimeCompare(sys.argv[2],sys.argv[3],sys.argv[4])
-        case "-usageproportional":
-            pass
-        #FUNCTION 2
+            waterUseTimeCompare(sys.argv[2], sys.argv[3], sys.argv[4])
         case "-percapita":
             if len(sys.argv) != 4:
                 print("Usage: python3 command_line.py perCapita --country --year")
                 return
             try:
                 value = get_per_capita_water_use(sys.argv[2], sys.argv[3])
-                print(f"{alias(sys.argv[2])}'s Water Usage per Capita: {round(value, 2)} Liters per day")
+                print(
+                    f"{alias(sys.argv[2])}'s Water Usage per Capita: {round(value, 2)} Liters per day"
+                )
             except ValueError as e:
                 print(e)
-        case "-usagepercentage":
-            print(get_usage_percentage(sys.argv[2],sys.argv[3],sys.argv[4]))
-            return
         case "-usageproportion":
             if len(sys.argv) != 4:
-                print("Usage: python3 command_line.py -usageproportion --country --year")
+                print(
+                    "Usage: python3 command_line.py -usageproportion --country --year"
+                )
                 return
             try:
-                print(usageProportion(sys.argv[2],sys.argv[3]))
+                print(usageProportion(sys.argv[2], sys.argv[3]))
             except ValueError as e:
                 print(e)
         case _:
@@ -67,23 +74,39 @@ def main():
 
 
 def openDB(database: DB):
+    """Returns an array for the spesificed database. EG: openDB(DB.AQS_DS3)"""
     if database not in list(DB):
         raise KeyError
-        
-    '''Returns an array for the spesificed database. EG: openDB(DB.AQS_DS3)'''
     arr = []
-    with open(database.value,newline='') as csvfile:
-        reader = csv.reader(csvfile,delimiter = ',',quotechar="|")  
+    with open(database.value, newline="") as csvfile:
+        reader = csv.reader(csvfile, delimiter=",", quotechar="|")
         for row in reader:
             arr.append(row)
-        return(arr)    
+        return arr
 
 
-def waterUseTimeCompare(country: str,year1: int,year2: int): ##Work in progress. Pulls from the wrong dataset right now.
+def waterUseTimeCompare(country: str, year1: int, year2: int):
+    """Compares the water use of a country between 2 years"""
     country = alias(country)
 
-    time1 = filterTagsDB(DB.AQS_WR,[str(country),str(year1),"Exploitable water resources and dam capacity","Total exploitable water resources"])
-    time2 = filterTagsDB(DB.AQS_WR,[str(country),str(year2),"Exploitable water resources and dam capacity","Total exploitable water resources"])
+    time1 = filterTagsDB(
+        DB.AQS_WR,
+        [
+            str(country),
+            str(year1),
+            "Exploitable water resources and dam capacity",
+            "Total exploitable water resources",
+        ],
+    )
+    time2 = filterTagsDB(
+        DB.AQS_WR,
+        [
+            str(country),
+            str(year2),
+            "Exploitable water resources and dam capacity",
+            "Total exploitable water resources",
+        ],
+    )
 
     if len(time1) == 0 or len(time2) == 0:
         raise KeyError
@@ -92,25 +115,25 @@ def waterUseTimeCompare(country: str,year1: int,year2: int): ##Work in progress.
 
     water_use_y1 = time1[6].strip()
     water_use_y2 = time2[6].strip()
-    print("Water usage in "+country,"\n")
-    print(str(year1)+": "+water_use_y1+"x10^9 cubic meters/year")
-    print(str(year2)+": "+water_use_y2+"x10^9 cubic meters/year")
+    print("Water usage in " + country, "\n")
+    print(str(year1) + ": " + water_use_y1 + "x10^9 cubic meters/year")
+    print(str(year2) + ": " + water_use_y2 + "x10^9 cubic meters/year")
     print("Difference:")
-    print(str(int(water_use_y2)-int(water_use_y1))+"x10^9 cubic meters/year")
+    print(str(int(water_use_y2) - int(water_use_y1)) + "x10^9 cubic meters/year")
 
-def usageProportion(country,year):
-    '''Returns the proportial usage of Agricultural, Industrial and Household water usage in terms of percentage'''
 
-    agc_percent = get_usage_percentage(country,year,"Agricultural")
-    ind_percent = get_usage_percentage(country,year,"Industrial")
-    hsh_percent = get_usage_percentage(country,year,"Household")
+def usageProportion(country, year):
+    """Returns the proportial usage of Agricultural, Industrial and Household water usage in terms of percentage"""
 
-    print("Water usage in "+country+" in "+year+"\n")
+    agc_percent = get_usage_percentage(country, year, "Agricultural")
+    ind_percent = get_usage_percentage(country, year, "Industrial")
+    hsh_percent = get_usage_percentage(country, year, "Household")
+
+    print("Water usage in " + country + " in " + year + "\n")
     print("Agricultural:", round(agc_percent, 2), "%")
     print("Industrial:", round(ind_percent, 2), "%")
     print("Household:", round(hsh_percent, 2), "%")
 
-    
 
 def alias(var: str) -> str:
     """Used to make it so that country names don't have to be input perfectly."""
@@ -123,10 +146,14 @@ def alias(var: str) -> str:
             return var
 
 
-def filterByTags(db:[],tags: []): 
+def filterByTags(db: [], tags: []):
     """"""
     arr = []
-    for row in db: # How this works: For every row, assume it by default matches the requested tags. For all requested tags, check if it's in the row. If it's not, set matches to false, and break the loop. If it matches all, adds the row to the array.
+    for (
+        row
+    ) in (
+        db
+    ):  # How this works: For every row, assume it by default matches the requested tags. For all requested tags, check if it's in the row. If it's not, set matches to false, and break the loop. If it matches all, adds the row to the array.
         matches = True
         for tag in tags:
             if tag not in row:
@@ -134,19 +161,19 @@ def filterByTags(db:[],tags: []):
                 break
         if matches:
             arr.append(row)
-            
+
     return arr
+
 
 def filterTagsDB(database: DB, tags: []):
     """Takes a database (enum) and an array of string tags. Returns all matches from the spesified DB. EG: filterByTagsDB(DB.CLEANED_GWC,['USA','2001'])"""
     arr = openDB(database)
-    return filterByTags(arr,tags)
+    return filterByTags(arr, tags)
+
 
 def get_per_capita_water_use(country: str, year: str) -> float:
-    '''Returns per capita water use (liters per day) for a given country and year'''
-
-    '''Raises ValueError if country/year not found or year out of range'''
-    if not year.isdigit() or not (2000 <= int(year) <= 2024):
+    """Returns per capita water use (liters per day) for a given country and year, Raises ValueError if country/year not found or year out of range"""
+    if not year.isdigit() or not 2000 <= int(year) <= 2024:
         raise ValueError("Year must be between 2000 and 2024.")
 
     country = alias(country)
@@ -154,19 +181,21 @@ def get_per_capita_water_use(country: str, year: str) -> float:
 
     # Skip header row
     for row in data[1:]:
-        if row[0] == country and row[1] == year: # match country and year
+        if row[0] == country and row[1] == year:  # match country and year
             try:
                 return float(row[3])  # 4th column for per capita water use
             except ValueError:
                 raise ValueError("Per capita value is missing or invalid.")
 
-    raise ValueError("Country or year not found. Pick another country or pick years from 2000-2024.")
+    raise ValueError(
+        "Country or year not found. Pick another country or pick years from 2000-2024."
+    )
+
 
 def get_usage_percentage(country: str, year: str, usagetype) -> float:
-    '''Returns percentage for usage for a given country and year'''
+    """Returns percentage for usage for a given country and year. Raises ValueError if country/year not found or year out of range"""
 
-    '''Raises ValueError if country/year not found or year out of range'''
-    if not year.isdigit() or not (2000 <= int(year) <= 2024):
+    if not year.isdigit() or not 2000 <= int(year) <= 2024:
         raise ValueError("Year must be between 2000 and 2024.")
 
     country = alias(country)
@@ -174,32 +203,28 @@ def get_usage_percentage(country: str, year: str, usagetype) -> float:
 
     # Skip header row
     for row in data[1:]:
-        if row[0] == country and row[1] == year: # match country and year
+        if row[0] == country and row[1] == year:  # match country and year
             if usagetype == "Agricultural":
                 try:
-                    return float(row[4])  
+                    return float(row[4])
                 except ValueError:
                     raise ValueError("Value is missing or invalid.")
             if usagetype == "Industrial":
                 try:
-                    return float(row[5])  
+                    return float(row[5])
                 except ValueError:
                     raise ValueError("Value is missing or invalid.")
             if usagetype == "Household":
                 try:
-                    return float(row[6])  
+                    return float(row[6])
                 except ValueError:
-                    raise ValueError("Value is missing or invalid.")    
+                    raise ValueError("Value is missing or invalid.")
 
-    raise ValueError("Country, year or usage type not found. " \
-    "Pick another country or pick years from 2000-2024 and make sure you are inputting 'Agriculture', 'Industrial' or 'Household'.")
+    raise ValueError(
+        "Country, year or usage type not found. "
+        "Pick another country or pick years from 2000-2024 and make sure you are inputting 'Agriculture', 'Industrial' or 'Household'."
+    )
 
 
-
-
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
-
-
-
-
